d766733d64ad9adef5061374a0d3c88cea066679
JAEEUN KIM <home@gimjaeeun-ui-MacBookAir.local>
2025-10-13 21:00:21 +0900

    Refactor UART output in test.c to directly write character 'H' and remove unused print_uart0 function

--- 1014_qemu/arm-baremetal/test.c
+++ 1014_qemu/arm-baremetal/test.c
@@ -1,10 +1,5 @@
-volatile unsigned int * const UART0DR = (unsigned int *)0x101f1000;
-void print_uart0(const char *s) {
-    while(*s != '\0') {
-        *UART0DR = (unsigned int)(*s);
-        s++;
-    }
-}
 void c_entry() {
-    print_uart0("Hello world!\n");
+    volatile unsigned int *p = (volatile unsigned int *)0x101f1000;
+    *p = 'H';  // UART0에 문자 'H' 강제 출력
+    while (1); // 무한 루프
 }
